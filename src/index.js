f0433f576d2688a9278ae14be94f88e8e9d46585
jiangyuan <jiangyuanlucky@gmail.com>
2016-09-08 00:09:35 +0800

    chore: init project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * @author jero
+ * @date 2016-09-07
+ */
+
+
+const a = 3;
---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+/**
+ * @author jero
+ * @date 2016-09-07
+ */
+
+import { expect } from 'chai';
+
+describe('validate-commit-user', () => {
+  it('test', () => {
+    expect(1).to.eq(1);
+  });
+});
